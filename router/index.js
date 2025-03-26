30fe83c6fd8c2f84b35087ce09c02aac3888920c
Juan Aramburo <2020030187@upsin.edu.mx>
2025-03-26 15:29:54 -0700

    Modificacion de servidor

--- router/index.js
+++ router/index.js
@@ -1,8 +1,12 @@
 import express,{Router} from "express";
 import json from "body-parser";
+
+import path from "path";
+import { fileURLToPath } from "url";
 import {sumar,restar,PI, alumnos} from "../modules/librerias.js"
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 router.get('/',(req,res)=>{
@@ -16,6 +20,10 @@ router.get('/1',(req,res)=>{
 
 router.get('/api/alumnos',(req,res) =>{
     res.json(alumnos);
+})
+
+router.get('/getapi',(req,res) =>{
+    res.sendFile(path.join(__dirname, "../getApi.html"))
 })
 
 router.get('/api/alumnos/:idAlumno',(req,res) =>{
